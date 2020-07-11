5582ffab9da14322f4b5720de5e4dd63d7b6567e
Phet <rawkidz@hotmail.com>
2020-07-11 15:07:14 +0700

    add some file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CoreModule } from './core/core.module';
+import { StoreModule, ActionReducer, MetaReducer } from '@ngrx/store';
+import { appReducers } from './state/app-reducer';
+import { environment } from '../environments/environment.prod';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+// for debugging
+export function debug(reducer: ActionReducer<any>): ActionReducer<any> {
+  return function(state, action) {
+    console.log('state', state); 
+    return reducer(state, action);
+  };
+}
 
+export const metaReducers: MetaReducer<any>[] = [debug];
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +27,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    CoreModule,
+    BrowserAnimationsModule,
+    StoreModule.forRoot(appReducers,{ 
+      metaReducers,
+      runtimeChecks:{
+        strictStateImmutability: false,
+        strictActionImmutability: false,
+      }
+    }),
+    EffectsModule.forRoot([]),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+    StoreRouterConnectingModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/state/app-reducer.ts
@@ -0,0 +1,9 @@
+import { ActionReducerMap } from '@ngrx/store';
+
+export interface AppState {
+
+}
+
+export const appReducers: ActionReducerMap<AppState> = {
+
+}
